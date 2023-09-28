66e14817dc18c443f9bd3fd63eeb8b1282588b89
Natalia Repkina <41197830+Nuttik@users.noreply.github.com>
2023-09-28 19:48:06 +0300

    2.2 image galery (#36)
    * fix: bags arter review
    
    * fix: scroll list rented books
    
    * init: start task 1.2-audio-player
    
    * feat: add basic player functionality and design
    
    * faet: add progress-bar html+css
    
    * feat: progress input move js
    
    * fix: fix bug move progress field
    
    * fix: html layout
    
    * feat: add change bg
    
    * feat: add favicon
    
    * fix: js
    
    * feat: add self assessment
    
    * init: start task 2.2-image-galery
    
    * feat: add basic layout and design
    
    * feat: add  function add img
    
    * feat: add andom generation
    
    * fix: press key for seach
    
    * feat: add modal windows
    
    * feat: add error message
    
    * fix: random page
    
    * fix: claer brunch
    
    * Merge branch 'gh-pages'
    
    * feat: add autocomplete="off"

---
+++ 2.2-image-galery/assets/js/image-galery.js
@@ -0,0 +1,140 @@
+//---Элементы DOM---//
+const search = document.getElementById("search");
+const input = search.querySelector("input");
+const searchButton = search.querySelector(".search__icon");
+const galery = document.getElementById("galery");
+const buttonMore = galery.querySelector(".button");
+const galeryImageList = galery.querySelector(".galery__img-list");
+const modal = document.getElementById("galery-modal");
+const modalContainer = modal.querySelector(".modal__container");
+
+//---Глобальные переменные---//
+let query;
+let count = 12;
+let moreCount = 9;
+let pageNumber;
+
+//---Функции---//
+function addInputClassActive() {
+  input.addEventListener("focus", function () {
+    search.classList.add("active");
+  });
+  input.addEventListener("blur", function () {
+    search.classList.remove("active");
+  });
+}
+
+function startSeach() {
+  if (input.value != "") {
+    query = input.value;
+  } else {
+    query = "random";
+  }
+
+  getData();
+}
+
+async function increaseCount() {
+  //сделать проверку, что с этой страници еще не было загружено фотографий
+  console.log(pageNumber);
+  pageNumber += 6;
+  console.log(pageNumber);
+  const url =
+    "https://api.unsplash.com/search/photos?query=" +
+    query +
+    "&per_page=" +
+    moreCount +
+    "&page=" +
+    pageNumber +
+    "&orientation=landscape&client_id=Z5ocwIy6zQdB2J-rFSJY20wNoPDYrqUXiJAWFviTUfQ";
+
+  const join = await fetch(url);
+  const data = await join.json();
+
+  createImage(data);
+  openModal();
+  closeModal();
+}
+
+function createImage(data) {
+  if (data.results.length < 1) {
+    let message = `<p class="galery__message">Oooops... Nothing was found for your request.</p>`;
+    galeryImageList.insertAdjacentHTML("beforeend", message);
+    buttonMore.disabled = true;
+  } else {
+    for (let i = 0; i < data.results.length; i++) {
+      const urlSmallImg = data.results[i].urls.small;
+      const imgAlt = data.results[i].alt_description;
+      const img = `<li class="galery__item"><img class="galery__img" src=${urlSmallImg} alt="${imgAlt}" title="Click to enlarge"></li>`;
+      galeryImageList.insertAdjacentHTML("beforeend", img);
+      buttonMore.disabled = false;
+    }
+  }
+}
+
+function clearImageList() {
+  galeryImageList.innerHTML = "";
+}
+
+async function getData() {
+  pageNumber = Math.floor(Math.random() * 30);
+  const url =
+    "https://api.unsplash.com/search/photos?query=" +
+    query +
+    "&per_page=" +
+    count +
+    "&page=" +
+    pageNumber +
+    "&orientation=landscape&client_id=Z5ocwIy6zQdB2J-rFSJY20wNoPDYrqUXiJAWFviTUfQ";
+  const join = await fetch(url);
+  const data = await join.json();
+
+  clearImageList();
+  createImage(data);
+  openModal();
+  closeModal();
+
+  //путь к миниатюре data.results[i].urls.small;
+  //путь к среднему размеру data.results[1].urls.regular
+}
+
+function openModal() {
+  const imageList = document.querySelectorAll(".galery__img");
+  imageList.forEach(function (img) {
+    img.addEventListener("click", function (event) {
+      modal.classList.remove("hidden");
+      let url = event.target.src.replace("&w=400", "&w=1080");
+      let alt = event.target.alt;
+      let img = `<img class="modal__img" src=${url} alt="${alt}" />`;
+      modalContainer.insertAdjacentHTML("beforeend", img);
+    });
+  });
+}
+
+function closeModal() {
+  const butonClose = modal.querySelector(".modal__closeButton");
+  modal.addEventListener("click", function (event) {
+    if (event.target == butonClose || !event.target.closest(".modal__window")) {
+      modal.classList.add("hidden");
+      modalContainer.innerHTML = "";
+    }
+  });
+}
+
+//let event = new Event("focus");
+//elem.dispatchEvent(event);
+
+//---вызов стартовых функций---//
+startSeach();
+addInputClassActive();
+buttonMore.addEventListener("click", increaseCount);
+
+//поиск по клику на иконку
+searchButton.onclick = startSeach;
+
+//поиск по нажатию на 'enter'
+document.addEventListener("keyup", function (event) {
+  if (event.code === "Enter") {
+    startSeach();
+  }
+});

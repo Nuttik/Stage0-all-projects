cb85e13a29a870065c34c133cd9117f811e562a1
Natalia Repkina <pn.88@mail.ru>
2023-10-07 17:10:39 +0300

    feat: add snake draw and move

---
+++ 3.3-random-game/assets/js/game-snake.js
@@ -0,0 +1,262 @@
+// -- Элементы DOM ---
+let canvas = document.getElementById("game-field");
+let startButtons = document.querySelectorAll(".button-play");
+let startButtonMain = document.getElementById("button-start");
+let gameWrapper = document.getElementById("game");
+let contrlsButtons = document.querySelector(".contrls__buttons");
+let buttonUp = document.getElementById("buttonUp");
+let buttonDown = document.getElementById("buttonDown");
+let buttonLeft = document.getElementById("buttonLeft");
+let buttonRight = document.getElementById("buttonRight");
+
+let ctx = canvas.getContext("2d");
+
+// --- Глобальные переменные ---
+let isPlay = false;
+let isPause = false;
+
+let elemSize = 64;
+let tick;
+let snake = {
+  parts: [
+    { x: 6, y: 6 },
+    { x: 5, y: 6 },
+    { x: 4, y: 6 },
+    { x: 3, y: 6 },
+  ],
+  direction: "East", // возможные направления "East", "North", "West", "South"
+};
+
+// --- Отрисовка элементов игры ----
+
+function drawElem(elem, src, cropX, cropY) {
+  let x = elem.x * elemSize;
+  let y = elem.y * elemSize;
+  var img = new Image(); // Создаёт новый элемент изображения
+  img.src = src; // Устанавливает путь
+  img.addEventListener(
+    "load",
+    function () {
+      ctx.drawImage(
+        img,
+        cropX,
+        cropY,
+        elemSize,
+        elemSize,
+        x,
+        y,
+        elemSize,
+        elemSize
+      );
+    },
+    false
+  );
+}
+
+function drawSnake() {
+  snake.parts.forEach((item, index, array) => {
+    if (index == 0) {
+      switch (snake.direction) {
+        case "East":
+          drawElem(item, "assets/images/sprite-snake.svg", 240, 0);
+          break;
+
+        case "North":
+          drawElem(item, "assets/images/sprite-snake.svg", 0, 0);
+          break;
+
+        case "West":
+          drawElem(item, "assets/images/sprite-snake.svg", 80, 0);
+          break;
+
+        case "South":
+          drawElem(item, "assets/images/sprite-snake.svg", 160, 0);
+          break;
+      }
+    } else if (index == array.length - 1) {
+      if (item.x + 1 == array[index - 1].x) {
+        drawElem(item, "assets/images/sprite-snake.svg", 240, 240);
+      }
+      if (item.x - 1 == array[index - 1].x) {
+        drawElem(item, "assets/images/sprite-snake.svg", 80, 240);
+      }
+      if (item.y + 1 == array[index - 1].y) {
+        drawElem(item, "assets/images/sprite-snake.svg", 0, 240);
+      }
+      if (item.y - 1 == array[index - 1].y) {
+        drawElem(item, "assets/images/sprite-snake.svg", 160, 240);
+      }
+    } else {
+      //идет по прямой
+      if (item.x + 1 == array[index - 1].x && item.y == array[index - 1].y) {
+        drawElem(item, "assets/images/sprite-snake.svg", 240, 320);
+      }
+      if (item.x - 1 == array[index - 1].x && item.y == array[index - 1].y) {
+        drawElem(item, "assets/images/sprite-snake.svg", 80, 320);
+      }
+      if (item.y + 1 == array[index - 1].y && item.x == array[index - 1].x) {
+        drawElem(item, "assets/images/sprite-snake.svg", 0, 320);
+      }
+      if (item.y - 1 == array[index - 1].y && item.x == array[index - 1].x) {
+        drawElem(item, "assets/images/sprite-snake.svg", 160, 320);
+      }
+      //поворачивает
+      //угол нижний правый
+      if (
+        (item.x + 1 == array[index + 1].x &&
+          item.y + 1 == array[index - 1].y) ||
+        (item.x + 1 == array[index - 1].x && item.y + 1 == array[index + 1].y)
+      ) {
+        drawElem(item, "assets/images/sprite-snake.svg", 0, 400);
+      }
+      //угол верхний левый
+      if (
+        (item.x - 1 == array[index + 1].x &&
+          item.y - 1 == array[index - 1].y) ||
+        (item.x - 1 == array[index - 1].x && item.y - 1 == array[index + 1].y)
+      ) {
+        drawElem(item, "assets/images/sprite-snake.svg", 80, 400);
+      }
+      // угол нижний левый
+      if (
+        (item.x - 1 == array[index - 1].x &&
+          item.y + 1 == array[index + 1].y) ||
+        (item.x - 1 == array[index + 1].x && item.y + 1 == array[index - 1].y)
+      ) {
+        drawElem(item, "assets/images/sprite-snake.svg", 160, 400);
+      }
+      //угол верхний правый
+      if (
+        (item.x + 1 == array[index - 1].x &&
+          item.y - 1 == array[index + 1].y) ||
+        (item.x + 1 == array[index + 1].x && item.y - 1 == array[index - 1].y)
+      ) {
+        drawElem(item, "assets/images/sprite-snake.svg", 240, 400);
+      }
+    }
+  });
+}
+
+function drawGame() {
+  drawSnake();
+}
+
+// Движение змейки
+function nextLocation() {
+  let snakeHead = snake.parts[0];
+  let nextX = snakeHead.x;
+  let nextY = snakeHead.y;
+  if (snake.direction === "East") {
+    nextX++;
+  } else if (snake.direction === "North") {
+    nextY++;
+  } else if (snake.direction === "West") {
+    nextX--;
+  } else if (snake.direction === "South") {
+    nextY--;
+  }
+  return { x: nextX, y: nextY };
+}
+
+function moveSnake() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  snake.parts.unshift(nextLocation());
+  snake.parts.pop();
+  drawSnake();
+}
+
+// Управление змейкой
+// клавиатура
+function controlSnakeWithKey(event) {
+  if (isPlay == true) {
+    if (event.code == "ArrowUp" || event.code == "KeyW") {
+      snake.direction = "South";
+      event.preventDefault();
+    } else if (event.code == "ArrowRight" || event.code == "KeyD") {
+      snake.direction = "East";
+      event.preventDefault();
+    } else if (event.code == "ArrowDown" || event.code == "KeyS") {
+      snake.direction = "North";
+      event.preventDefault();
+    } else if (event.code == "ArrowLeft" || event.code == "KeyA") {
+      snake.direction = "West";
+      event.preventDefault();
+    }
+  }
+}
+// по клику на стрелки на экране
+function controlSnakeWithButton(event) {
+  if (isPlay == true) {
+    if (event.target == buttonUp) {
+      snake.direction = "South";
+    } else if (event.target == buttonRight) {
+      snake.direction = "East";
+    } else if (event.target == buttonDown) {
+      snake.direction = "North";
+    } else if (event.target == buttonLeft) {
+      snake.direction = "West";
+    }
+  }
+}
+
+// тики для бесконечного движения змейки
+function startTick(func) {
+  tick = setInterval(func, 500);
+}
+function stopTick() {
+  clearTimeout(tick);
+}
+
+function startGame() {
+  isPlay = true;
+  startTick(moveSnake);
+}
+
+// -- Кнопки
+
+function clickButtonPlay(event) {
+  let button = event.target;
+  gameWrapper.classList.remove("game_before-start");
+  button.classList.add("hidden");
+
+  startGame();
+}
+
+startButtons.forEach((button) => {
+  button.addEventListener("click", clickButtonPlay);
+});
+
+function pressEnterStart(event) {
+  if (
+    event.key == "Enter" &&
+    gameWrapper.classList.contains("game_before-start")
+  ) {
+    gameWrapper.classList.remove("game_before-start");
+    startButtonMain.classList.add("hidden");
+    startGame();
+  }
+}
+function pressSpasePause(event) {
+  if (
+    event.code == "Space" &&
+    !gameWrapper.classList.contains("game_before-start") &&
+    isPause == false
+  ) {
+    gameWrapper.classList.add("game_pause");
+    stopTick();
+    isPause = true;
+    event.preventDefault();
+  }
+  if (event.code == "Space" && isPause == true) {
+    gameWrapper.classList.remove("game_pause");
+    startTick(moveSnake);
+    isPause = false;
+  }
+}
+
+// --- Вызов функций ---
+
+document.addEventListener("keydown", controlSnakeWithKey);
+contrlsButtons.addEventListener("click", controlSnakeWithButton);
+document.addEventListener("keydown", pressEnterStart);
+document.addEventListener("keydown", pressSpasePause);

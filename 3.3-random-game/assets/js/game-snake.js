3cdf8cbdd4a3e525fd81ee05818d0022feafcdbc
Natalia Repkina <pn.88@mail.ru>
2023-10-08 18:52:12 +0300

    feat: add crash snake

--- 3.3-random-game/assets/js/game-snake.js
+++ 3.3-random-game/assets/js/game-snake.js
@@ -1,34 +1,28 @@
 // -- Элементы DOM ---
-let canvas = document.getElementById("game-field");
-let startButtons = document.querySelectorAll(".button-play");
-let startButtonMain = document.getElementById("button-start");
-let gameWrapper = document.getElementById("game");
-let contrlsButtons = document.querySelector(".contrls__buttons");
-let buttonUp = document.getElementById("buttonUp");
-let buttonDown = document.getElementById("buttonDown");
-let buttonLeft = document.getElementById("buttonLeft");
-let buttonRight = document.getElementById("buttonRight");
+const canvas = document.getElementById("game-field");
+const startButtons = document.querySelectorAll(".button-play");
+const startButtonMain = document.getElementById("button-start");
+const gameWrapper = document.getElementById("game");
+const contrlsButtons = document.querySelector(".contrls__buttons");
+const buttonUp = document.getElementById("buttonUp");
+const buttonDown = document.getElementById("buttonDown");
+const buttonLeft = document.getElementById("buttonLeft");
+const buttonRight = document.getElementById("buttonRight");
+const gameOverPopUp = document.getElementById("game-over");
 
-let ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
 // --- Глобальные переменные ---
 let isPlay = false;
+let isCrashSnake = false;
 let score;
 let lives;
-
 let elemSize = 64;
 let tick;
-let snake = {
-  parts: [
-    { x: 5, y: 6 },
-    { x: 4, y: 6 },
-    { x: 3, y: 6 },
-  ],
-  direction: "East", // возможные направления "East", "North", "West", "South"
-};
+let imgSrc;
+let snake;
 
 // --- Отрисовка элементов игры ----
-
 function drawElem(elem, src, cropX, cropY) {
   let x = elem.x * elemSize;
   let y = elem.y * elemSize;
@@ -53,24 +47,41 @@ function drawElem(elem, src, cropX, cropY) {
   );
 }
 
-function drawSnake() {
+function drawSnake(imgSrc) {
   snake.parts.forEach((item, index, array) => {
     if (index == 0) {
       switch (snake.direction) {
         case "East":
-          drawElem(item, "assets/images/sprite-snake.svg", 240, 0);
+          if (isCrashSnake == true) {
+            drawElem(item, "assets/images/sprite-snake.svg", 240, 80);
+          } else {
+            drawElem(item, "assets/images/sprite-snake.svg", 240, 0);
+          }
+
           break;
 
         case "North":
-          drawElem(item, "assets/images/sprite-snake.svg", 0, 0);
+          if (isCrashSnake == true) {
+            drawElem(item, "assets/images/sprite-snake.svg", 0, 80);
+          } else {
+            drawElem(item, "assets/images/sprite-snake.svg", 0, 0);
+          }
           break;
 
         case "West":
-          drawElem(item, "assets/images/sprite-snake.svg", 80, 0);
+          if (isCrashSnake == true) {
+            drawElem(item, "assets/images/sprite-snake.svg", 80, 80);
+          } else {
+            drawElem(item, "assets/images/sprite-snake.svg", 80, 0);
+          }
           break;
 
         case "South":
-          drawElem(item, "assets/images/sprite-snake.svg", 160, 0);
+          if (isCrashSnake == true) {
+            drawElem(item, "assets/images/sprite-snake.svg", 160, 80);
+          } else {
+            drawElem(item, "assets/images/sprite-snake.svg", 160, 0);
+          }
           break;
       }
     } else if (index == array.length - 1) {
@@ -138,6 +149,15 @@ function drawSnake() {
 }
 
 // Движение змейки
+
+// тики для бесконечного движения змейки
+function startTick(func) {
+  tick = setInterval(func, 500);
+}
+function stopTick() {
+  clearInterval(tick);
+}
+
 function nextLocation() {
   let snakeHead = snake.parts[0];
   let nextX = snakeHead.x;
@@ -163,34 +183,85 @@ function moveSnake() {
       rect.y * elemSize + elemSize
     );
   });
-  snake.parts.unshift(nextLocation());
-  snake.parts.pop();
+  let next = nextLocation();
+  if (isNextWall(next) || isNextTail(next)) {
+    crashSnake();
+  } else {
+    snake.parts.unshift(next);
+    snake.parts.pop();
+  }
   drawSnake();
 }
 
-// тики для бесконечного движения змейки
-function startTick(func) {
-  tick = setInterval(func, 500);
-}
-function stopTick() {
-  clearTimeout(tick);
+// Змейка врезается в препятствие
+function crashSnake() {
+  stopTick();
+  lives = lives - 1;
+  isCrashSnake = true;
+  console.log(lives, score);
+
+  if (lives > 0) {
+    setTimeout(function () {
+      startGame(lives, score);
+    }, 1500);
+  }
+  if (lives == 0) {
+    gameOverPopUp.classList.remove("hidden");
+  }
 }
 
-// Управление змейкой
+function isNextWall(next) {
+  if (
+    next.x < 0 ||
+    next.y < 0 ||
+    next.x * elemSize == canvas.width ||
+    next.y * elemSize == canvas.height
+  ) {
+    return true;
+  } else return false;
+}
+function isNextTail(next) {
+  let result = false;
+  snake.parts.map((item) => {
+    if (item.x === next.x && item.y === next.y) {
+      result = true;
+    }
+  });
+  return result;
+}
+
+// Управление змейкой ------------------------ ЗАПРЕТИТЬ ОБРАТНЫЙ ХОД
 // клавиатура
 function controlSnakeWithKey(event) {
   if (isPlay == true) {
-    if (event.code == "ArrowUp" || event.code == "KeyW") {
+    if (
+      (event.code == "ArrowUp" || event.code == "KeyW") &&
+      snake.direction != "North"
+    ) {
       snake.direction = "South";
-      event.preventDefault();
-    } else if (event.code == "ArrowRight" || event.code == "KeyD") {
+    } else if (
+      (event.code == "ArrowRight" || event.code == "KeyD") &&
+      snake.direction != "West"
+    ) {
       snake.direction = "East";
-      event.preventDefault();
-    } else if (event.code == "ArrowDown" || event.code == "KeyS") {
+    } else if (
+      (event.code == "ArrowDown" || event.code == "KeyS") &&
+      snake.direction != "South"
+    ) {
       snake.direction = "North";
-      event.preventDefault();
-    } else if (event.code == "ArrowLeft" || event.code == "KeyA") {
+    } else if (
+      (event.code == "ArrowLeft" || event.code == "KeyA") &&
+      snake.direction != "East"
+    ) {
       snake.direction = "West";
+    }
+
+    if (
+      event.code == "ArrowUp" ||
+      event.code == "ArrowRight" ||
+      event.code == "ArrowDown" ||
+      event.code == "ArrowLeft"
+    ) {
       event.preventDefault();
     }
   }
@@ -198,13 +269,13 @@ function controlSnakeWithKey(event) {
 // по клику на стрелки на экране
 function controlSnakeWithButton(event) {
   if (isPlay == true) {
-    if (event.target == buttonUp) {
+    if (event.target == buttonUp && snake.direction != "North") {
       snake.direction = "South";
-    } else if (event.target == buttonRight) {
+    } else if (event.target == buttonRight && snake.direction != "West") {
       snake.direction = "East";
-    } else if (event.target == buttonDown) {
+    } else if (event.target == buttonDown && snake.direction != "South") {
       snake.direction = "North";
-    } else if (event.target == buttonLeft) {
+    } else if (event.target == buttonLeft && snake.direction != "East") {
       snake.direction = "West";
     }
   }
@@ -213,11 +284,24 @@ document.addEventListener("keydown", controlSnakeWithKey);
 contrlsButtons.addEventListener("click", controlSnakeWithButton);
 
 // -- Старт игры
-function startGame() {
+function startGame(countLives, countScore) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  snake = {
+    parts: [
+      { x: 5, y: 5 },
+      { x: 4, y: 5 },
+      { x: 3, y: 5 },
+    ],
+    direction: "East", // возможные направления "East", "North", "West", "South"
+  };
   isPlay = true;
-  lives = 3;
-  score = 0;
+  isCrashSnake = false;
+  lives = countLives;
+  score = countScore;
+
+  if (!gameOverPopUp.classList.contains("hidden")) {
+    gameOverPopUp.classList.add("hidden");
+  }
 
   startTick(moveSnake);
 }
@@ -225,9 +309,9 @@ function startGame() {
 function clickButtonPlay(event) {
   let button = event.target;
   gameWrapper.classList.remove("game_before-start");
-  button.classList.add("hidden");
+  startButtonMain.classList.add("hidden");
 
-  startGame();
+  startGame(3, 0);
 }
 
 startButtons.forEach((button) => {
@@ -241,7 +325,7 @@ function pressEnterStart(event) {
   ) {
     gameWrapper.classList.remove("game_before-start");
     startButtonMain.classList.add("hidden");
-    startGame();
+    startGame(3, 0);
   }
 }
 document.addEventListener("keydown", pressEnterStart);

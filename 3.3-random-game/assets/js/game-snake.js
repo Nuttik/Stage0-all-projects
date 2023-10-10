07b4f15d93a72c7729a64deaf9b777993a83da63
Natalia Repkina <pn.88@mail.ru>
2023-10-10 15:37:46 +0300

    feat: add draw fruits

--- 3.3-random-game/assets/js/game-snake.js
+++ 3.3-random-game/assets/js/game-snake.js
@@ -1,3 +1,10 @@
+/* Баги: 
+ -- змейка мигает из-за очищения ее тела при отрисовки перемещения
+ -- фрукты мигают. из-за разных сетинтервалов их затирает стирание теле змейки при перемещении. 
+ Но иногда очищается и фрукт на позиции где змейка еще не была, 
+ НО при отключении очистки тела змейки фрукты перестают исчезать 
+*/
+
 // -- Элементы DOM ---
 const canvas = document.getElementById("game-field");
 const startButtons = document.querySelectorAll(".button-play");
@@ -15,12 +22,14 @@ const ctx = canvas.getContext("2d");
 // --- Глобальные переменные ---
 let isPlay = false;
 let isCrashSnake = false;
+let intervalAddFruit;
 let score;
 let lives;
 let elemSize = 64;
 let tick;
 let imgSrc;
 let snake;
+let fruits;
 
 // --- Отрисовка элементов игры ----
 function drawElem(elem, src, cropX, cropY) {
@@ -148,15 +157,40 @@ function drawSnake(imgSrc) {
   });
 }
 
-// Движение змейки
+function drawAllFruit() {
+  for (let fruit in fruits) {
+    if (fruits[fruit].length > 0) {
+      fruits[fruit].forEach((coord) => {
+        switch (fruit) {
+          case "apples":
+            drawElem(coord, "assets/images/sprite-snake.svg", 0, 480);
+            break;
+
+          case "pears":
+            drawElem(coord, "assets/images/sprite-snake.svg", 80, 480);
+            break;
+
+          case "bananas":
+            drawElem(coord, "assets/images/sprite-snake.svg", 160, 480);
+            break;
+        }
+      });
+    }
+  }
 
 // тики для бесконечного движения змейки
-function startTick(func) {
-  tick = setInterval(func, 500);
+function startTick() {
+  tick = setInterval(function () {
+    moveSnake();
+    drawSnake();
+    drawAllFruit();
+  }, 300);
 }
 function stopTick() {
   clearInterval(tick);
 }
+
+// Движение змейки
 
 function nextLocation() {
   let snakeHead = snake.parts[0];
@@ -176,6 +210,7 @@ function nextLocation() {
 
 function moveSnake() {
   snake.parts.forEach((rect) => {
+    //ЗАТИРАЕТ ФРУКТЫ!!!!!!!!!!!
     ctx.clearRect(
       rect.x * elemSize,
       rect.y * elemSize,
@@ -183,6 +218,7 @@ function moveSnake() {
       rect.y * elemSize + elemSize
     );
   });
+
   let next = nextLocation();
   if (isNextWall(next) || isNextTail(next)) {
     crashSnake();
@@ -190,7 +226,6 @@ function moveSnake() {
     snake.parts.unshift(next);
     snake.parts.pop();
   }
-  drawSnake();
 }
 
 // Змейка врезается в препятствие
@@ -198,7 +233,6 @@ function crashSnake() {
   stopTick();
   lives = lives - 1;
   isCrashSnake = true;
-  console.log(lives, score);
 
   if (lives > 0) {
     setTimeout(function () {
@@ -208,6 +242,7 @@ function crashSnake() {
   if (lives == 0) {
     gameOverPopUp.classList.remove("hidden");
   }
+  clearInterval(intervalAddFruit); //остановка отрисовки фруктов
 }
 
 function isNextWall(next) {
@@ -230,7 +265,72 @@ function isNextTail(next) {
   return result;
 }
 
-// Управление змейкой ------------------------ ЗАПРЕТИТЬ ОБРАТНЫЙ ХОД
+// Фрукты
+
+function newFruit() {
+  //выбираем какой фрукт добавляем из объекта fruit
+  let num = Math.floor(Math.random() * 3);
+  let typeFruit = Object.keys(fruits)[num];
+
+  //выбираем рандомное свободное место на поле (где нет змейки)
+  let fruitLocation = randomFreeCoordinates(typeFruit);
+
+  //пушим координаты в объект фруктов по нужному типу
+  fruits[typeFruit].push(fruitLocation);
+
+  //возвращаем массив [тип фрукта, позицию]
+  return [typeFruit, fruitLocation];
+}
+
+function randomFreeCoordinates(typeFruit) {
+  //выбираем рандомное свободное место на поле (где нет змейки)
+  let maxX = 10;
+  let maxY = 10;
+  let randX = Math.floor(Math.random() * maxX + 1);
+  let randY = Math.floor(Math.random() * maxY + 1);
+  let isFree = true;
+
+  snake.parts.forEach((part) => {
+    if (part.x === randX && part.y === randY) {
+      isFree = false;
+    }
+  });
+  if (isFree == true) {
+    for (key in fruits) {
+      fruits[key].forEach((fruit) => {
+        if (fruit.x === randX && fruit.y === randY) {
+          isFree = false;
+        }
+      });
+    }
+  }
+  return isFree === true
+    ? { x: randX, y: randY }
+    : randomFreeCoordinates(typeFruit);
+}
+function drawNewFruit() {
+  let fruit = newFruit();
+  switch (fruit[0]) {
+    case "apples":
+      drawElem(fruit[1], "assets/images/sprite-snake.svg", 0, 480);
+      break;
+
+    case "pears":
+      drawElem(fruit[1], "assets/images/sprite-snake.svg", 80, 480);
+      break;
+
+    case "bananas":
+      drawElem(fruit[1], "assets/images/sprite-snake.svg", 160, 480);
+      break;
+  }
+}
+function addFruit() {
+  if (isPlay === true && isCrashSnake === false) {
+    intervalAddFruit = setInterval(drawNewFruit, 2000); //СТИРАЕТСЯ ИЗ_ЗА СЕТИНТЕРВАЛА ТАМ, ГДЕ ЗМЕЙКА БЫЛА 4 СЕКУНДЫ НАЗАД
+  }
+}
+
+// Управление змейкой
 // клавиатура
 function controlSnakeWithKey(event) {
   if (isPlay == true) {
@@ -294,6 +394,11 @@ function startGame(countLives, countScore) {
     ],
     direction: "East", // возможные направления "East", "North", "West", "South"
   };
+  fruits = {
+    apples: [],
+    pears: [],
+    bananas: [],
+  };
   isPlay = true;
   isCrashSnake = false;
   lives = countLives;
@@ -303,7 +408,8 @@ function startGame(countLives, countScore) {
     gameOverPopUp.classList.add("hidden");
   }
 
-  startTick(moveSnake);
+  startTick();
+  addFruit();
 }
 
 function clickButtonPlay(event) {
